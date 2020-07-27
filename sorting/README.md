d19e521d0866dd083f081cc716d85b6718db7e21
Michael O'Keefe <michael.okeefe@semanticbits.com>
2020-07-27 08:43:41 -0400

    Started sorting section

---
+++ sorting/README.md
@@ -0,0 +1,13 @@
+# Sorting
+
+https://www.youtube.com/watch?v=kPRA0W1kECg
+https://www.toptal.com/developers/sorting-algorithms
+visualgo.net
+
+Built in array.sort
+Default sort order is according to string unicode code points
+
+## Bubble sort
+For each item, compare it to the next item in the list, if item is larger than the next, swap them, then check that same
+item against the next
+repeat until no swaps done
---
+++ sorting/bubbleSort.js
@@ -0,0 +1,48 @@
+function bubbleSort(arr) {
+ for (let i = arr.length - 1; i >= 0; i --) {
+     let swapsDone = false;
+     for (let j = 0; j < i; j ++) {
+         if (arr[j] > arr[j + 1]) {
+             console.log('swapping', arr[j], 'with', arr[j + 1]);
+             swap(arr, j, j + 1);
+             swapsDone = true;
+         }
+     }
+     // Optimization to avoid extra looping
+     if (!swapsDone) {
+         break;
+     }
+ }
+ return arr;
+}
+
+function swap(arr, i1, i2) {
+    [ arr[i1], arr[i2] ] = [ arr[i2], arr[i1] ]
+}
+
+console.log(bubbleSort([ 5,3,11,6,8,9 ]));
+
+// first round
+// i = 9
+// j = 5
+// j + 1 = 3
+// SWAP [ 3,5,11,6,8,9 ]
+// i = 9
+// j = 5
+// j + 1 = 11
+// i = 9
+// j = 11
+// j + 1 = 6
+// SWAP [ 3,5,6,11,8,9 ]
+// i = 9
+// j = 11
+// j + 1 = 8
+// SWAP [ 3,5,6,8,11,9 ]
+// i = 9
+// j = 11
+// j + 1 = 9
+// SWAP [ 3,5,6,8,9,11 ]
+// i = 9
+// j = 3
+// j + 1 = 5
+// At this point we keep going until i = 0 and break!
