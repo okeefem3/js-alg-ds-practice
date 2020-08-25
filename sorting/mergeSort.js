1bc00f7c738b8cc33111a7877255c445de7deef4
Michael O'Keefe <michael.okeefe@semanticbits.com>
2020-08-25 15:17:14 -0400

    Finished merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,9 +1,30 @@
 function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const half = Math.ceil(arr.length / 2);
+    return mergeArrays(mergeSort(arr.slice(0, half)), arr.slice(half));
+}
 
+function mergeArrays(n, m) {
+    let i = 0;
+    let j = 0;
+    const merged = [];
+    while (i < n.length || j < m.length) {
+        if (j >= m.length || n[i] < m[j]) {
+            merged.push(n[i]);
+            i++;
+        } else {
+            merged.push(m[j]);
+            j++;
+        }
+    }
+    return merged;
 }
 
 
 console.log(mergeSort([ 5,3,11,6,8,9 ]));
+// console.log(mergeArrays([ 5,3,11 ], [ 6,8,9 ]));
 
 // split into [5,3] [11,6] [8,9]
 // split into [5] [3] [11] [6] [8] [9]
